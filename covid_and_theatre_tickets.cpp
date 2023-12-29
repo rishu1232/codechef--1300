1eb634d9874fc80a23005a950b2000dd8e80735a
rishu1232 <154673865+rishu1232@users.noreply.github.com>
2023-12-29 22:09:10 +0530

    Create covid_and_theatre_tickets.cpp

---
+++ covid_and_theatre_tickets.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--){
+		int m,n;
+		cin >> m >> n;
+		cout << (n+1)/2 * ((m+1)/2) << endl;
+	}
+return 0;
+}
